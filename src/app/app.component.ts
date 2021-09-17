6a260038ab9e71491cd7f8c0ee89271d001b450b
Mwiza Kumwenda <mwiza.kumwenda@virinco.com>
2021-09-17 16:33:14 +0200

    Initialize repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IgxGridComponent } from 'igniteui-angular';
+import { athletesData } from './sample-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public localData: any = athletesData;
+
+  ngOnInit(): void {
+    this.localData = athletesData;
+  }
+
   title = 'ignite-ui';
+  @ViewChild('myGrid', { read: IgxGridComponent })
+  public grid!: IgxGridComponent;
 }
